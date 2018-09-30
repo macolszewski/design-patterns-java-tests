9dbd4c735de57014bb744d45f4d23a890aecad12
macolszewski <olschevski@gmail.com>
2018-09-30 10:21:05 +0200

    pełnomocnik

--- src/designpatterns/proxy/Manager.java
+++ src/designpatterns/proxy/Manager.java
@@ -4,7 +4,37 @@ public class Manager implements Employee {
 
     @Override
     public void generateTimeReport() {
+        System.out.print("Generuje raport");
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                int i = 0;
+                String kropa = ".";
+                while (true) {
+                    if (i < 8) {
+                        try {
+                            Thread.sleep(500);
+                            System.out.print(kropa);
+                            i++;
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    } else {
+                        System.out.println();
+                        break;
+                    }
+                }
+                this.interrupt();
+            }
+        };
+        thread.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("Generuję raport dla Managera!");
+        System.out.println();
     }
 
 
